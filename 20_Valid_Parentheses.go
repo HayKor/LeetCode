cb0404de4a8f5927b80f00214f0bc93ecea39669
Arthur <baginian.artur@yandex.com>
2024-09-29 15:54:48 +0300

    valid parentheses with Golang

---
+++ 20_Valid_Parentheses.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isValid(s string) bool {
+	stack := []rune{}
+	for _, sym := range s {
+		switch sym {
+		case '(', '[', '{':
+			stack = append(stack, sym)
+		default:
+			if len(stack) == 0 ||
+				(sym == ')' && stack[len(stack)-1] != '(') ||
+				(sym == '}' && stack[len(stack)-1] != '{') ||
+				(sym == ']' && stack[len(stack)-1] != '[') {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(isValid("()()()()"))
+	fmt.Println(isValid("())"))
+}
